955b25b3f6c83b404c40334774dd5d94a566b333
Mike <mikej.miller440@gmail.com>
2022-01-23 09:10:30 -0600

    moved generateMarkdown to src

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // packages
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./src/generateMarkdown');
 
 // initial questions prompt
 const initialQuestions = () => {
